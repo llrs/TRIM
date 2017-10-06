11574ff34a5f34264e12fe2908db8ecab752f67f
llrs <lluis.revilla@gmail.com>
2017-10-06 10:59:25 +0200

    Remove samples from stools for which we don't have intestinal samples

--- cleaning.R
+++ cleaning.R
@@ -67,11 +67,9 @@ meta_i <- meta_i[order(meta_i$Patient_ID, meta_i$Time), ]
 comPatient <- intersect(meta_i$Patient_ID, meta_s$Patient_ID)
 comTime <- intersect(meta_i$Time, meta_s$Time)
 
-
-# Keep only the common patients
+# Keep only the common patients and times
 com_meta_i <- meta_i[meta_i$Patient_ID %in% comPatient & meta_i$Time %in% comTime, ]
 com_meta_s <- meta_s[meta_s$Patient_ID %in% comPatient & meta_s$Time %in% comTime, ]
-
 
 # Samples per patient and Time
 tab_s <- table(com_meta_s$Patient_ID, com_meta_s$Time)
@@ -79,19 +77,52 @@ tab_i <- table(com_meta_i$Patient_ID, com_meta_i$Time)
 
 # Compare if we have more intestinal samples than stools for the same time and
 # patient
-moreS <- tab_i > tab_s & tab_s != 0
+moreS <- tab_i > tab_s & tab_i != 0 & tab_s != 0
+QmoreS <- tab_i - tab_s
 
-# Duplicate rows/data for those that we have stools but not as much as 
-# intestinal samples
+# Delete rows of patients which are not in common between the datasets
+keep_i <- rownames(meta_i)[meta_i$Patient_ID %in% comPatient & 
+                             meta_i$Time %in% comTime]
+keep_s <- rownames(meta_s)[meta_s$Patient_ID %in% comPatient & 
+                             meta_s$Time %in% comTime]
+com_otus_table_i <- otus_table_i[, keep_i]
+com_otus_table_s <- otus_table_s[, keep_s]
+
+# Extract the ids of the data we need to add or remove
+library("reshape2")
+df <- melt(QmoreS, varnames = c("Patient_ID", "Time"))
+df <- df[df$value != 0, ]
+
+# Remove the stools that should be removed because we don't have intestinal 
+# samples
+remove_s <- df[df$value <0, ]
+rownames(remove_s) <- seq_len(nrow(remove_s))
+
+remove_samples_s <- apply(remove_s, 1, function(x){
+  rownames(meta_s)[meta_s$Patient_ID %in% x[1] & meta_s$Time %in% x[2]]
+})
+
+com_otus_table_s <- com_otus_table_s[, 
+                                     !colnames(com_otus_table_s) %in% remove_samples_s]
+
+# Add stool samples as much as need
+add_s <- df[df$value >0, ]
+rownames(add_s) <- seq_len(nrow(add_s))
+
+remove_samples_s <- apply(add_s, 1, function(x){
+  rownames(meta_s)[meta_s$Patient_ID %in% x[1] & meta_s$Time %in% x[2]]
+})
+
+com_otus_table_s <- com_otus_table_s[, 
+                                     !colnames(com_otus_table_s) %in% remove_samples_s]
 
 
-
-# Integrate them
-library("RGCCA")
-A <- list(stools = otus_table_s, intestinal = otus_table_i, metadata = )
-C <- matrix(ncol = 2, nrow = 2)
-sgcca.glioma = sgcca(A, C, c1 = c(.071,.2, 1),
-                     ncomp = c(1, 1, 1),
-                     scheme = "centroid",
-                     scale = TRUE,
-                     verbose = FALSE)
+# # Integrate them
+# library("RGCCA")
+# A <- list(stools = otus_table_s, intestinal = otus_table_i, metadata = )
+# C <- matrix(ncol = 2, nrow = 2)
+# sgcca.glioma = sgcca(A, C, c1 = c(.071,.2, 1),
+#                      ncomp = c(1, 1, 1),
+#                      scheme = "centroid",
+#                      scale = TRUE,
+#                      verbose = FALSE)
