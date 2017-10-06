638aa49af7e2b36d3037d99ac75fabf737489a8b
llrs <lluis.revilla@gmail.com>
2017-10-06 17:21:04 +0200

    Analyse the stool and the intestinal 16S

---
+++ stool_intestinal_integration.R
@@ -0,0 +1,42 @@
+library("RGCCA")
+source("helper_functions.R")
+
+otus_i <- read.csv(file = "intestinal_16S/otus_coherent.csv")
+otus_s <- read.csv(file = "stools_16S/otus_coherent.csv")
+
+A <- list(stools = otus_s, intestinal = otus_i)
+C <- matrix(0, ncol = 2, nrow = 2, dimnames = list(names(A), names(A)))
+
+subSymm <- function(m, x, y, val){
+  m[x, y] <- val
+  m[y, x] <- val
+  m
+}
+C <- subSymm(C, "intestinal", "stools", 1)
+# C <- subSymm(C, "metadata", "stools", 1)
+# C <- subSymm(C, "metadata", "intestinal", 1)
+
+(shrinkage <- sapply(A, tau.estimate))
+
+
+
+sgcca.centroid <-  sgcca(A, C, c1 = shrinkage,
+                     ncomp = c(1, 1),
+                     scheme = "centroid",
+                     scale = TRUE,
+                     verbose = FALSE)
+
+sgcca.factorial <-  sgcca(A, C, c1 = shrinkage,
+                         ncomp = c(1, 1),
+                         scheme = "factorial",
+                         scale = TRUE,
+                         verbose = FALSE)
+
+sgcca.horst <-  sgcca(A, C, c1 = shrinkage,
+                         ncomp = c(1, 1, 1),
+                         scheme = "horst",
+                         scale = TRUE,
+                         verbose = FALSE)
+
+list(sgcca.centroid = sgcca.centroid, sgcca.horst = sgcca.horst, 
+     sgcca.factorial = sgcca.factorial)
