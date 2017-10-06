0c07a8efb270e2c72b56875bcaea8e294f85a331
llrs <lluis.revilla@gmail.com>
2017-10-06 15:47:44 +0200

    Match datasets and reorder them

--- cleaning.R
+++ cleaning.R
@@ -71,15 +71,6 @@ comTime <- intersect(meta_i$Time, meta_s$Time)
 com_meta_i <- meta_i[meta_i$Patient_ID %in% comPatient & meta_i$Time %in% comTime, ]
 com_meta_s <- meta_s[meta_s$Patient_ID %in% comPatient & meta_s$Time %in% comTime, ]
 
-# Samples per patient and Time
-tab_s <- table(com_meta_s$Patient_ID, com_meta_s$Time)
-tab_i <- table(com_meta_i$Patient_ID, com_meta_i$Time)
-
-# Compare if we have more intestinal samples than stools for the same time and
-# patient
-moreS <- tab_i > tab_s & tab_i != 0 & tab_s != 0
-QmoreS <- tab_i - tab_s
-
 # Delete rows of patients which are not in common between the datasets
 keep_i <- rownames(meta_i)[meta_i$Patient_ID %in% comPatient & 
                              meta_i$Time %in% comTime]
@@ -88,14 +79,26 @@ keep_s <- rownames(meta_s)[meta_s$Patient_ID %in% comPatient &
 com_otus_table_i <- otus_table_i[, keep_i]
 com_otus_table_s <- otus_table_s[, keep_s]
 
+
+# Samples per patient and Time
+tab_s <- table(com_meta_s$Patient_ID, com_meta_s$Time)
+tab_i <- table(com_meta_i$Patient_ID, com_meta_i$Time)
+
+# Compare if we have more intestinal samples than stools for the same time and
+# patient
+moreS <- tab_i > tab_s & tab_s != 0
+QmoreS <- tab_i - tab_s
+
 # Extract the ids of the data we need to add or remove
 library("reshape2")
-df <- melt(QmoreS, varnames = c("Patient_ID", "Time"))
-df <- df[df$value != 0, ]
+meltQ <- melt(QmoreS, varnames = c("Patient_ID", "Time"))
+meltL <- melt(moreS, varnames = c("Patient_ID", "Time"))
+remove_s <- meltQ[meltQ$value < 0, ]
+add_s <- meltQ[meltQ$value > 0 & meltL$value,]
+remove_i <- meltQ[meltQ$value > 0 & !meltL$value,]
 
 # Remove the stools that should be removed because we don't have intestinal 
 # samples
-remove_s <- df[df$value <0, ]
 rownames(remove_s) <- seq_len(nrow(remove_s))
 
 remove_samples_s <- apply(remove_s, 1, function(x){
@@ -106,23 +109,49 @@ com_otus_table_s <- com_otus_table_s[,
                                      !colnames(com_otus_table_s) %in% remove_samples_s]
 
 # Add stool samples as much as need
-add_s <- df[df$value >0, ]
 rownames(add_s) <- seq_len(nrow(add_s))
 
-remove_samples_s <- apply(add_s, 1, function(x){
-  rownames(meta_s)[meta_s$Patient_ID %in% x[1] & meta_s$Time %in% x[2]]
+add_samples_s <- apply(add_s, 1, function(x){
+  rep(rownames(meta_s)[meta_s$Patient_ID %in% x[1] & meta_s$Time %in% x[2]], x[3])
 })
 
 com_otus_table_s <- com_otus_table_s[, 
-                                     !colnames(com_otus_table_s) %in% remove_samples_s]
+                                     c(colnames(com_otus_table_s), unlist(add_samples_s))]
 
+# Remove intestinal data
+rownames(remove_i) <- seq_len(nrow(remove_i))
 
+remove_samples_i <- apply(remove_i, 1, function(x){
+  rownames(meta_i)[meta_i$Patient_ID %in% x[1] & meta_i$Time %in% x[2]]
+})
+
+com_otus_table_i <- com_otus_table_i[, 
+                                 !colnames(com_otus_table_i) %in% unlist(remove_samples_i)]
+
+# Reorder so that the stools samples and the intestinal samples are in the 
+# same order including Time and Patient_ID
+
+char_com_i <- meta_i[colnames(com_otus_table_i), c("Patient_ID", "Time")]
+
+names_clean <- gsub("(.+)\\.[0-9]$", "\\1", colnames(com_otus_table_s))
+char_com_s <- meta_s[names_clean, c("Patient_ID", "Time")]
+o <- order(char_com_s$Patient_ID, char_com_i$Time)
+
+otus_s <- com_otus_table_s[, o]
+otus_i <- com_otus_table_s
+
+# The number of columns and order!!! of the samples must match
+# ncol(s) == ncol(i) s$Time == i$Time & s$Patient_ID == i$Patient_ID
+# Before going to RGCCA
 # # Integrate them
-# library("RGCCA")
-# A <- list(stools = otus_table_s, intestinal = otus_table_i, metadata = )
-# C <- matrix(ncol = 2, nrow = 2)
-# sgcca.glioma = sgcca(A, C, c1 = c(.071,.2, 1),
-#                      ncomp = c(1, 1, 1),
-#                      scheme = "centroid",
-#                      scale = TRUE,
-#                      verbose = FALSE)
+library("RGCCA")
+# We transpose the data because it requires the data in column for variable, row
+# for sample
+A <- list(stools = t(otus_s), intestinal = t(otus_i), 
+          metadata = com_meta_i[colnames(otus_i),])
+C <- matrix(0, ncol = 3, nrow = 3, dimnames = list(names(A), names(A)))
+sgcca.glioma = sgcca(A, C, c1 = c(.071,.2, 1),
+                     ncomp = c(1, 1, 1),
+                     scheme = "centroid",
+                     scale = TRUE,
+                     verbose = FALSE)
