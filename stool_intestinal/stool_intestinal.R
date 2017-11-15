99bfa34af7915e37eb62a1f8ba7305de927c6c30
llrs <lluis.revilla@gmail.com>
2017-11-15 16:33:04 +0100

    Calculate the mean of the correlation between the same microorganisms

--- stool_intestinal/stool_intestinal.R
+++ stool_intestinal/stool_intestinal.R
@@ -14,6 +14,7 @@ eqOTUS <- read.csv("equivalent_otus.csv", stringsAsFactors = FALSE)
 setwd(cd)
 
 keep <- !grepl("28_T52_T_DM_CH", meta$Sample_Code) # Remove outlier See PCA
+
 meta <- meta[keep, ]
 otus_s <- otus_s[keep, ]
 otus_i <- otus_i[keep, ]
@@ -22,64 +23,125 @@ otus_i <- otus_i[keep, ]
 ta <- unique(tax_i[eqOTUS$intestinal, c("Genus", "Species")])
 ta <- apply(ta, 1, paste, collapse = " ")
 
-
-## species that are in both datasets
+## OTUs of the species in both datasets
 u_i <- unique(eqOTUS$intestinal)
 u_s <- unique(eqOTUS$stools)
 
+findCorOTUs <- function(x){
+  if (sum(x) == 0) {
+    return(NA)
+  } else {
+  cor(otus_i[x, u_i], otus_s[x, u_s], 
+      use = "pairwise.complete.obs", method = "spearman")
+  }
+}
+pdf(paste0("Figures/", today, "_correlations.pdf"))
+cors <- findCorOTUs(rep(TRUE, nrow(otus_i)))
+
+# Do the mean of those correlations that correspond to the same microorganism
+# before doing the boxplots
+corsOrg <- sapply(rownames(unique(tax_s[eqOTUS$stools,])), function(y){
+  
+  # Find all the OTUs in the intestinal
+  o_i <- eqOTUS$intestinal[eqOTUS$stools == y]
+  # Find all the otus of stools for the same org. 
+  o_s <- lapply(o_i, function(x){eqOTUS$stools[eqOTUS$intestinal == x]})
+  o_s <- unique(unlist(o_s))
+  
+  if (!y %in% o_s) {
+    stop(y, " should be on the list of otus ", paste(o_i, collapse = ", "))
+  }
+  
+  ta <- paste(tax_s[y, c("Genus", "Species")], collapse = " ")
+  mCors <- mean(cors[o_i, o_s], na.rm = TRUE)
+  c("All" = mCors, "ta" = ta)
+})
+corsOrg <- as.data.frame(t(corsOrg))
+corsOrg$All <- as.numeric(levels(corsOrg$All))[corsOrg$All] 
+ggplot(corsOrg, aes("All", y = All)) + 
+  geom_violin() + 
+  # geom_boxplot() +
+  geom_point(position = position_jitter()) +
+  xlab("Samples")
+  
+
 ### Compare the equivalent otus in different settings
 time_area <- allComb(meta, c("CD_Aftected_area"))
-subCors <- sapply(as.data.frame(time_area), function(x){
+subCors <- sapply(as.data.frame(time_area), findCorOTUs, simplify = FALSE)
+
+# Do the mean of those correlations that correspond to the same microorganism
+# before doing the boxplots
+cors2Org <- sapply(rownames(unique(tax_s[eqOTUS$stools,])), function(y){
+  
+  # Find all the OTUs in the intestinal
+  o_i <- eqOTUS$intestinal[eqOTUS$stools == y]
+  # Find all the otus of stools for the same org. 
+  o_s <- lapply(o_i, function(x){eqOTUS$stools[eqOTUS$intestinal == x]})
+  o_s <- unique(unlist(o_s))
+  
+  if (!y %in% o_s) {
+    stop(y, " should be on the list of otus ", paste(o_i, collapse = ", "))
+  }
+  
+  ta <- paste(tax_s[y, c("Genus", "Species")], collapse = " ")
+  test <- sapply(seq_along(subCors), function(xy) {
+    mOTUs <- mean(subCors[[xy]][o_i, o_s], na.rm = TRUE)
+    
+    names(mOTUs) <- names(subCors)[xy]
+    mOTUs
+  })
+  test
+})
+corsOrg <- cbind(corsOrg, t(cors2Org))
+ggplot(melt(corsOrg), aes(variable, y = value)) + 
+  # geom_violin() + 
+  geom_boxplot() +
+  geom_point(position = position_jitter()) +
+  xlab("Samples") + 
+  guides(col = FALSE)
+
+ggplot(corsOrg, aes("ratio", y = COLON/ILEUM)) + 
+  geom_violin() + 
+  # geom_boxplot() +
+  geom_point(aes(col = ta), position = position_jitter()) +
+  xlab("Samples") + 
+  guides(col = FALSE)
+
+### Compare the equivalent otus in different settings
+time <- allComb(meta, "Time")
+subCors <- sapply(as.data.frame(time), function(x){
   cor(otus_i[x, u_i], otus_s[x, u_s], use = "pairwise.complete.obs", 
       method = "spearman")
 }, simplify = FALSE)
 
-# Tranform the data for the plot
-cors <- sapply(subCors, function(x){
-  x <- x[upper.tri(x)]
-  x[!is.na(x)]
-})
-
-cors <- cors[colSums(time_area) >= 4]
-corEqOtus <- melt(cors)
-
-# Set the factors of Time in the order we want
-ggplot(corEqOtus) +
-  geom_boxplot(aes(L1, value)) + 
-  ylab("Correlation with stools") +
-  ggtitle("Comparison with stools") + 
-  xlab("CD afected area")
-
-# Do the mean of those correlations that correspond to the same microorganism
-# before doing the boxplots
-a <- sapply(rownames(unique(tax_s[eqOTUS$stools,])), function(y){
-  o_i <- eqOTUS$intestinal[eqOTUS$stools == y]
-  o_s <- lapply(o_i, function(x){
-    eqOTUS$stools[eqOTUS$intestinal == x]
-  })
-  o_s <- unique(unlist(o_s))
-  o_i <- lapply(o_s, function(x){
-    eqOTUS$intestinal[eqOTUS$stools == x]
-  })
-  o_i <- unique(unlist(o_i))
+cors2Org <- sapply(rownames(unique(tax_s[eqOTUS$stools,])), function(y){
   
-  d <- matrix(FALSE, ncol = ncol(subCors[[1]]), nrow = nrow(subCors[[1]]), 
-              dimnames = dimnames(subCors[[1]]))
-  d[o_i, o_s] <- TRUE
+  # Find all the OTUs in the intestinal
+  o_i <- eqOTUS$intestinal[eqOTUS$stools == y]
+  # Find all the otus of stools for the same org. 
+  o_s <- lapply(o_i, function(x){eqOTUS$stools[eqOTUS$intestinal == x]})
+  o_s <- unique(unlist(o_s))
+  
+  if (!y %in% o_s) {
+    stop(y, " should be on the list of otus ", paste(o_i, collapse = ", "))
+  }
+  
   ta <- paste(tax_s[y, c("Genus", "Species")], collapse = " ")
   test <- sapply(seq_along(subCors), function(xy) {
-    # heatmap(t(subCors[[xy]]), scale = "none", 
-    #         ylab = "OTUs stools", xlab = "OTUs intestinal",
-    #         Rowv = NA, Colv = NA,  
-    #         main = paste0("Correlation of ", ta, " in ", names(subCors)[xy]), 
-    #         add.expr = { makeRects(d,"blue")})
     mOTUs <- mean(subCors[[xy]][o_i, o_s], na.rm = TRUE)
-    list("Species" = ta, "mean_cor" = mOTUs, "cond" = names(subCors)[xy], 
-         "n" = sum(time_area[, xy], na.rm = TRUE))
+    
+    names(mOTUs) <- names(subCors)[xy]
+    mOTUs
   })
-  t(test)
+  test
 })
-
+corsOrg <- cbind(corsOrg, t(cors2Org))
+ggplot(melt(corsOrg), aes(variable, y = value)) + 
+  # geom_violin() + 
+  geom_boxplot() +
+  # geom_point(position = position_jitter()) +
+  xlab("Samples") + 
+  guides(col = FALSE)
 
 ### Compare the equivalent otus in different settings
 time_area <- allComb(meta, c("Time", "CD_Aftected_area"))
@@ -88,28 +150,34 @@ subCors <- sapply(as.data.frame(time_area), function(x){
       method = "spearman")
 }, simplify = FALSE)
 
-
-# Tranform the data for the plot
-cors <- sapply(subCors, function(x){
-  x <- x[upper.tri(x)]
-  x[!is.na(x)]
+cors2Org <- sapply(rownames(unique(tax_s[eqOTUS$stools,])), function(y){
+  
+  # Find all the OTUs in the intestinal
+  o_i <- eqOTUS$intestinal[eqOTUS$stools == y]
+  # Find all the otus of stools for the same org. 
+  o_s <- lapply(o_i, function(x){eqOTUS$stools[eqOTUS$intestinal == x]})
+  o_s <- unique(unlist(o_s))
+  
+  if (!y %in% o_s) {
+    stop(y, " should be on the list of otus ", paste(o_i, collapse = ", "))
+  }
+  
+  ta <- paste(tax_s[y, c("Genus", "Species")], collapse = " ")
+  test <- sapply(seq_along(subCors), function(xy) {
+    mOTUs <- mean(subCors[[xy]][o_i, o_s], na.rm = TRUE)
+    
+    names(mOTUs) <- names(subCors)[xy]
+    mOTUs
+  })
+  test
 })
-
-cors <- cors[colSums(time_area) >= 4]
-corEqOtus <- melt(cors)
-
-name <- strsplit(corEqOtus$L1, "_|_")
-corEqOtus$Time <- sapply(name, function(x){x[1]})
-corEqOtus$Site <- sapply(name, function(x){x[3]})
-# Set the factors of Time in the order we want
-corEqOtus$Time <- factor(corEqOtus$Time, 
-                         levels = c("T0", "TM36", "TM48", "T26", "T52", "T106"))
-
-ggplot(corEqOtus) +
-  geom_boxplot(aes(Site, value)) + 
-  facet_grid(~Time) +
-  ylab("Correlation with stools") +
-  ggtitle("Comparison with stools")
+corsOrg <- cbind(corsOrg, t(cors2Org))
+ggplot(melt(corsOrg), aes(variable, y = value)) + 
+  # geom_violin() + 
+  geom_boxplot() +
+  geom_point(position = position_jitter()) +
+  xlab("Samples") + 
+  guides(col = FALSE)
 
 ### Compare the equivalent otus in different settings
 time_responder <- allComb(meta, c("Time", "HSCT_responder"))
@@ -120,26 +188,41 @@ subCors <- sapply(as.data.frame(time_responder), function(x){
   cor(otus_i[x, u_i], otus_s[x, u_s], use = "pairwise.complete.obs", 
       method = "spearman")
 }, simplify = FALSE)
-
-
-# Tranform the data for the plot
-cors <- sapply(subCors, function(x){
-  x <- x[upper.tri(x)]
-  x[!is.na(x)]
+cors2Org <- sapply(rownames(unique(tax_s[eqOTUS$stools,])), function(y){
+  
+  # Find all the OTUs in the intestinal
+  o_i <- eqOTUS$intestinal[eqOTUS$stools == y]
+  # Find all the otus of stools for the same org. 
+  o_s <- lapply(o_i, function(x){eqOTUS$stools[eqOTUS$intestinal == x]})
+  o_s <- unique(unlist(o_s))
+  
+  if (!y %in% o_s) {
+    stop(y, " should be on the list of otus ", paste(o_i, collapse = ", "))
+  }
+  
+  ta <- paste(tax_s[y, c("Genus", "Species")], collapse = " ")
+  test <- sapply(seq_along(subCors), function(xy) {
+    if (is.null(dim(subCors[[xy]]))) {
+      return(NA)
+    }
+    mOTUs <- mean(subCors[[xy]][o_i, o_s], na.rm = TRUE)
+    
+    names(mOTUs) <- names(subCors)[xy]
+    mOTUs
+  })
+  test
 })
+cors2Org <- t(cors2Org)
+keep <- apply(cors2Org, 2, function(x){all(is.na(x))})
+cors2Org <- cors2Org[, !keep]
 
-cors <- cors[colSums(time_responder) >= 4]
-corEqOtus <- melt(cors)
+corsOrg <- cbind(corsOrg, cors2Org)
+ggplot(melt(corsOrg), aes(variable, y = value)) + 
+  # geom_violin() + 
+  geom_boxplot() +
+  geom_point(position = position_jitter()) +
+  xlab("Samples") + 
+  guides(col = FALSE)
 
-name <- strsplit(corEqOtus$L1, "_|_")
-corEqOtus$Time <- sapply(name, function(x){x[1]})
-corEqOtus$Responder <- sapply(name, function(x){x[3]})
-# Set the factors of Time in the order we want
-corEqOtus$Time <- factor(corEqOtus$Time, 
-                         levels = c("T0", "TM36", "TM48", "T26", "T52", "T106"))
-
-ggplot(corEqOtus) +
-  geom_boxplot(aes(Responder, value)) + 
-  facet_grid(~Time) +
-  ylab("Correlation with stools") +
-  ggtitle("Comparison with stools")
+dev.off() 
+save.image(file = paste0(today, "_stool_intestinal.RData"))
