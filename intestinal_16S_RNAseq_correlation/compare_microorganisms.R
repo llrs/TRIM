a6686611bd8bc703de348d52ebe816840b8dfe9b
llrs <lluis.revilla@gmail.com>
2018-04-19 17:04:33 +0200

    Update threshold

--- intestinal_16S_RNAseq_correlation/compare_microorganisms.R
+++ intestinal_16S_RNAseq_correlation/compare_microorganisms.R
@@ -25,18 +25,21 @@ names(reactome) <- mapIds(org.Hs.eg.db, keys= names(reactome), keytype = "ENTREZ
 reactome <- reactome[!is.na(names(reactome))]
 
 # Similarity between genes related to microorganisms
-groups <- cors_all[lengths(cors_all) >= 1]
+groups <- cors_all[lengths(cors_all) >= 10]
 comp <- mclusterGeneSim(groups, reactome, c("BMA", "BMA"))
 comp <- comp[is.finite(comp[1, ]), is.finite(comp[, 1])]
 
 # MDS and plotting
+mds <- cmdscale(as.dist(1-comp))
+# 
 png(paste0("Figures/", today, "_microorganisms_MDS.png"))
-mds <- cmdscale(comp)
-plot(mds*1.1, type = "n", xlab = "MDS1", ylab = "MDS2", main = "Microorganisms relation by function")
+plot(mds*1.2, type = "n", xlab = "MDS1", ylab = "MDS2", main = "Microorganisms relation by function", asp = 1)
+abline( h = 0, v = 0, col = "lightgrey")
 text(mds[, 1], mds[, 2], labels = rownames(mds))
 dev.off()
 png(paste0("Figures/", today, "_microorganisms_MDS_zoom.png"))
-sub_mds <- mds[abs(mds[, 1]) < 0.1, ]
-plot(sub_mds, type = "n", xlab = "MDS1", ylab = "MDS2", main = "Microorganisms relation by function")
+sub_mds <- mds[abs(mds[, 1]) < 0.25 & abs(mds[, 2]) < 0.25, ]
+plot(sub_mds, type = "n", xlab = "MDS1", ylab = "MDS2", main = "Microorganisms relation by function", asp = 1)
+abline( h = 0, v = 0, col = "lightgrey")
 text(sub_mds[, 1], sub_mds[, 2], labels = rownames(sub_mds))
 dev.off()
