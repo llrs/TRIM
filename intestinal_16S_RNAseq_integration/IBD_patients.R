3546df3782c9756019a15d5cfcc04803635489bb
llrs <lluis.revilla@gmail.com>
2018-07-06 10:32:16 +0200

    Integrating subset

---
+++ intestinal_16S_RNAseq_integration/IBD_patients.R
@@ -0,0 +1,354 @@
+cd <- setwd("..")
+library("fgsea")
+library("ggforce")
+# Load the helper file
+today <- format(Sys.time(), "%Y%m%d")
+library("integration")
+
+intestinal <- "intestinal_16S"
+rna <- "intestinal_RNAseq"
+
+
+# Read the intestinal otus table
+otus_table_i <- read.csv(
+  file.path(intestinal, "OTUs-Table-new-biopsies.csv"),
+  stringsAsFactors = FALSE, row.names = 1,
+  check.names = FALSE
+)
+tax_i <- otus_table_i[, ncol(otus_table_i)]
+otus_table_i <- otus_table_i[, -ncol(otus_table_i)]
+
+# Extract the taxonomy and format it properly
+otus_tax_i <- taxonomy(tax_i, rownames(otus_table_i))
+
+
+# Load the input data
+expr <- read.delim(file.path(rna, "taula_sencera2.tsv"), check.names = FALSE)
+
+# Read the metadata for each type of sample
+file_meta_i <- "intestinal_16S/db_biopsies_trim_seq16S_noBCN.txt"
+meta_i <- read.delim(
+  file_meta_i, row.names = 1, check.names = FALSE,
+  stringsAsFactors = FALSE
+)
+file_meta_r <- file.path(rna, "metadata_25042018.csv")
+meta_r <- read.delim(
+  file_meta_r, check.names = FALSE,
+  stringsAsFactors = FALSE, 
+  na.strings = c("NA", "")
+)
+
+setwd(cd)
+
+# Correct the swapped samples
+position <- c(grep("33-T52-TTR-CIA", colnames(expr)), 
+              grep("33-T52-TTR-IIA", colnames(expr)))
+colnames(expr)[position] <- colnames(expr)[rev(position)]
+colnames(expr) <- toupper(colnames(expr))
+
+# Correct the metadata
+meta_i <- meta_i_norm(meta_i)
+meta_r <- meta_r_norm(meta_r)
+
+# normalize names of samples
+colnames(otus_table_i) <- gsub("[0-9]+\\.(.+)$", "\\1", colnames(otus_table_i))
+
+# Check metadata with the names present in both datas
+meta_r <- meta_r[meta_r$Seq_code_uDNA %in% colnames(otus_table_i) &
+                   meta_r$`Sample Name_RNA` %in% colnames(expr), ]
+
+# Filter only the IBD patients
+meta_r <- meta_r[meta_r$IBD == "CD", ]
+
+# Subset the sequencing data
+expr <- expr[, meta_r$`Sample Name_RNA`]
+otus_table_i <- otus_table_i[, meta_r$Seq_code_uDNA]
+
+# Normalize expression
+expr_edge <- edgeR::DGEList(expr)
+expr_edge <- edgeR::calcNormFactors(expr_edge, method = "TMM")
+expr_norm <- edgeR::cpm(expr_edge, normalized.lib.sizes=TRUE, log = TRUE)
+
+# Filter expression
+expr <- norm_RNAseq(expr_norm)
+
+# Normalize OTUS
+library("metagenomeSeq")
+MR_i <- newMRexperiment(
+  otus_table_i, 
+  featureData = AnnotatedDataFrame(as.data.frame(otus_tax_i[rownames(otus_table_i), ]))
+)
+MR_i <- cumNorm(MR_i, metagenomeSeq::cumNormStat(MR_i))
+otus_table_i <- MRcounts(MR_i, norm = TRUE, log = TRUE)
+
+# Subset if all the rows are 0 and if sd is 0
+otus_table_i <- otus_table_i[apply(otus_table_i, 1, sd) != 0, ]
+
+# Prepare input for the sgcca function
+A <- list(RNAseq = t(expr), "16S" = t(otus_table_i))
+A <- sapply(A, function(x){
+  x[, apply(x, 2, sd) != 0]
+}, simplify = FALSE)
+
+saveRDS(A, file = "TRIM_IBD.RDS")
+
+# The design
+C <- matrix(
+  0, ncol = length(A), nrow = length(A),
+  dimnames = list(names(A), names(A))
+)
+C <- subSymm(C, "16S", "RNAseq", 1)
+
+# We cannnot comput eht tau.estimate for A[[1]]
+# (shrinkage <- sapply(A, tau.estimate))
+shrinkage <- c(0.25670333, 0) # We guess a 0.1 for the RNAseq expression
+shrinkage[2] <- tau.estimate(A[[2]])
+(min_shrinkage <- sapply(A, function(x) {
+  1 / sqrt(ncol(x))
+}))
+# # Don't let the shrinkage go below the thershold allowed
+(shrinkage <- ifelse(shrinkage < min_shrinkage, min_shrinkage, shrinkage))
+# shrinkage <- rep(1, length(A))
+
+ncomp <- rep(2, length(A))
+
+sgcca.centroid <- sgcca(
+  A, C, c1 = shrinkage,
+  ncomp = ncomp,
+  scheme = "centroid",
+  scale = TRUE,
+  verbose = FALSE
+)
+names(sgcca.centroid$Y) <- names(A)
+names(sgcca.centroid$a) <- names(A)
+names(sgcca.centroid$astar) <- names(A)
+names(sgcca.centroid$AVE$AVE_X) <- names(A)
+sgcca.centroid$AVE$AVE_X <- simplify2array(sgcca.centroid$AVE$AVE_X)
+
+saveRDS(sgcca.centroid, file = "IBD.RDS")
+
+# Find the direction of the correlation
+samples <- data.frame(
+  "RNAseq" = sgcca.centroid$Y[["RNAseq"]][, 1],
+  "microbiota" = sgcca.centroid$Y[["16S"]][, 1]
+)
+
+lmt <- lm(microbiota ~ RNAseq, data = samples)
+
+if (lmt$coefficients[2] > 0) {
+  d <- c(1, 1)
+} else if (lmt$coefficients[2] < 0) {
+  d <- c(-1, -1)
+}
+
+## Grouping of the variables ####
+RNAseq1 <- samples$RNAseq
+RNAseq2 <- sgcca.centroid$Y[["RNAseq"]][, 2]
+microbiota2 <- sgcca.centroid$Y[["16S"]][, 2]
+microbiota1 <- samples$microbiota
+
+names(RNAseq1) <- rownames(samples)
+names(microbiota1) <- rownames(samples)
+names(RNAseq2) <- rownames(samples)
+names(microbiota2) <- rownames(samples)
+groups <- split(rownames(samples), as.factor(meta_r$HSCT_responder))
+# First dimension seems to capture well the
+fgsea(groups, RNAseq1, nperm = 1000)
+fgsea(groups, microbiota1, nperm = 1000)
+# Further dimensions
+fgsea(groups, RNAseq2, nperm = 1000)
+fgsea(groups, microbiota2, nperm = 1000)
+
+km <- kmeans(samples[, c("RNAseq", "microbiota")], 2, nstart = 2)
+plot(samples[, c("RNAseq", "microbiota")], col = km$cluster)
+
+## Plotting results ####
+# Colors for the plots
+names(colors) <- unique(meta_r$ID)
+
+samples <- cbind(samples, droplevels(meta_r))
+samples$Patient_ID <- as.factor(samples$Patient_ID)
+samples$Sample_Code <- as.character(samples$Sample_Code)
+
+pdf(paste0("Figures/", today, "_RGCCA_plots_IBD.pdf"))
+
+# Labels of the samples
+label <- strsplit(as.character(samples$`Sample Name_RNA`), split = "-")
+labels <- sapply(label, function(x) {
+  if (length(x) == 5) {
+    x[5]
+  }
+  else if (length(x) != 5) {
+    x[4]
+  }
+})
+
+samples <- cbind(samples, labels)
+samples$Time <- factor(samples$Time, levels(as.factor(samples$Time))[c(1, 2, 4, 5, 3, 6, 7, 8)])
+
+
+comm <- ggplot(samples, aes(RNAseq, microbiota)) + # It is really biopsies
+  geom_vline(xintercept = 0) +
+  geom_hline(yintercept = 0) +
+  ggtitle("All samples at all times ") +
+  xlab("RNAseq (component 1)") +
+  ylab("16S (component 1)") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_abline(intercept = 0, slope = d[1], linetype = 2)
+
+
+for (p in seq_along(levels(samples$Time))) {
+  a <- comm +
+    geom_text(aes(color = ID, label = ID)) +
+    guides(col = guide_legend(title = "Patient")) +
+    scale_color_manual(values = colors) +
+    facet_wrap_paginate(~Time, ncol = 1, nrow = 1, page = p)
+  print(a)
+}
+
+for (p in seq_along(levels(samples$ID))) {
+  a <- comm +
+    geom_text(aes(color = ID, label = ifelse(!is.na(labels),
+                                             paste(Time, labels, sep = "_"),
+                                             as.character(Time)
+    ))) +
+    guides(col = guide_legend(title = "Patient")) +
+    scale_color_manual(values = colors) +
+    facet_wrap_paginate(~ID, ncol = 1, nrow = 1, page = p)
+  print(a)
+}
+comm +
+  geom_text(aes(
+    color = ID,
+    label = ifelse(!is.na(labels),
+      paste(Time, labels, sep = "_"),
+      as.character(Time)
+    )
+  )) +
+  guides(col = guide_legend(title = "Patient")) +
+  scale_color_manual(values = colors)
+
+
+comm +
+  geom_text(aes(
+    color = HSCT_responder,
+    label = paste(ID, labels, sep = "_")
+  )) +
+  guides(col = guide_legend(title = "Responders"))
+
+
+comm +
+  geom_text(aes(
+    color = Endoscopic_Activity,
+    label = ifelse(!is.na(labels),
+      paste(ID, labels, sep = "_"),
+      as.character(ID)
+    )
+  )) +
+  guides(col = guide_legend(title = "Endoscopic Activity"))
+
+comm +
+  geom_text(aes(color = Time, label = ifelse(!is.na(labels),
+    paste(ID, labels, sep = "_"),
+    as.character(ID)
+  ))) +
+  guides(col = guide_legend(title = "Time"))
+
+variables <- data.frame(
+  Origin = rep(names(A), sapply(A, ncol)),
+  comp1 = unlist(sapply(
+    sgcca.centroid$a,
+    function(x) {
+      x[, 1]
+    }
+  )),
+  comp2 = unlist(sapply(
+    sgcca.centroid$a,
+    function(x) {
+      x[, 2]
+    }
+  ))
+)
+variables$var <- gsub("^.*\\.(OTU_.*)$", "\\1", rownames(variables))
+rownames(variables) <- gsub("^.*\\.(OTU_.*)$", "\\1", rownames(variables))
+variables$var <- gsub("^RNAseq\\.(ENSG.*)$", "\\1", rownames(variables))
+rownames(variables) <- gsub("^.*\\.(ENSG.*)$", "\\1", rownames(variables))
+rownames(variables) <- gsub("^metadata\\.(.*)$", "\\1", rownames(variables))
+variables$var <- gsub("^metadata\\.(.*)$", "\\1", rownames(variables))
+
+# Remove the variables that in both components are 0
+keepComp1RNAseq <- mean(abs(variables$comp1)[variables$Origin == "RNAseq"])
+keepComp1_16S <- mean(abs(variables$comp1)[variables$Origin == "16S"])
+
+keepComp2RNAseq <- mean(abs(variables$comp2)[variables$Origin == "RNAseq"])
+keepComp2_16S <- mean(abs(variables$comp2)[variables$Origin == "16S"])
+
+keepComp1 <- c(
+  abs(variables$comp1[variables$Origin == "RNAseq"]) > keepComp1RNAseq,
+  abs(variables$comp1[variables$Origin == "16S"]) > keepComp1_16S
+)
+keepComp2 <- c(
+  abs(variables$comp2[variables$Origin == "RNAseq"]) > keepComp2RNAseq,
+  abs(variables$comp2[variables$Origin == "16S"]) > keepComp2_16S
+)
+
+subVariables <- variables[keepComp1 & keepComp2, ]
+
+ggplot(subVariables, aes(comp1, comp2), color = Origin) +
+  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.1, npoints = 100)) +
+  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.2, npoints = 100)) +
+  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.3, npoints = 100)) +
+  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.4, npoints = 100)) +
+  geom_text(aes(color = Origin, label = var)) +
+  geom_vline(xintercept = 0) +
+  geom_hline(yintercept = 0) +
+  coord_cartesian() +
+  ggtitle(
+    "Variables important for the first two components",
+    subtitle = "Integrating stools and mucosa samples"
+  )
+
+rnaseq_i <- subVariables$var[subVariables$Origin == "RNAseq"]
+if (length(rnaseq_i) >= 2) {
+  pr <- prcomp(t(expr[rnaseq_i, ]), scale. = TRUE)
+  prS <- summary(pr)
+  ggplot(as.data.frame(pr$x), aes(PC1, PC2, color = as.factor(meta_r$HSCT_responder))) +
+    geom_point() +
+    guides(col = guide_legend(title = "Responder")) +
+    xlab(paste("PC1", prS$importance[2, "PC1"] * 100)) +
+    ylab(paste("PC2", prS$importance[2, "PC2"] * 100)) +
+    ggtitle("RNAseq PCA from the important variables")
+}
+
+micro_i <- subVariables$var[subVariables$Origin == "16S"]
+if (length(micro_i) >= 2) {
+  pr <- prcomp(t(otus_table_i[micro_i, ]), scale. = TRUE)
+  prS <- summary(pr)
+  ggplot(as.data.frame(pr$x), aes(PC1, PC2, color = as.factor(meta_r$HSCT_responder))) +
+    geom_point() +
+    guides(col = guide_legend(title = "Responder")) +
+    xlab(paste("PC1", prS$importance[2, "PC1"] * 100)) +
+    ylab(paste("PC2", prS$importance[2, "PC2"] * 100)) +
+    ggtitle("16S PCA from the important variables")
+}
+# Plot for the same component the variables of each block
+comp1 <- sapply(sgcca.centroid$a, function(x) {
+  x[, 1]
+})
+variables_weight(comp1)
+
+# Second component
+comp2 <- sapply(sgcca.centroid$a, function(x) {
+  x[, 2]
+})
+variables_weight(comp2)
+
+# Bootstrap of sgcca
+STAB <- boot_sgcca(A, C, shrinkage, 1000)
+
+saveRDS(STAB, file = "bootstrap_IBD.RDS")
+
+# Evaluate the boostrap effect and plot
+boot_evaluate(STAB)
+
+dev.off()
---
+++ intestinal_16S_RNAseq_integration/biological_relations.R
@@ -0,0 +1,89 @@
+# Test if the loadings of the genes are from a relevant pathways.
+cd <- setwd("..")
+
+# Load the helper file
+today <- format(Sys.time(), "%Y%m%d")
+library("integration")
+
+intestinal <- "intestinal_16S"
+rna <- "intestinal_RNAseq"
+
+
+epithelium <- read.csv("epithelium.csv")
+epithelium <- epithelium$Epithelium
+
+# Read the intestinal otus table
+otus_table_i <- read.csv(
+  file.path(intestinal, "OTUs-Table-new-biopsies.csv"),
+  stringsAsFactors = FALSE, row.names = 1,
+  check.names = FALSE
+)
+tax_i <- otus_table_i[, ncol(otus_table_i)]
+otus_table_i <- otus_table_i[, -ncol(otus_table_i)]
+
+# Extract the taxonomy and format it properly
+otus_tax_i <- taxonomy(tax_i, rownames(otus_table_i))
+
+# Load the input data
+expr <- read.delim(file.path(rna, "taula_sencera2.tsv"), check.names = FALSE)
+
+# Read the metadata for each type of sample
+file_meta_i <- "intestinal_16S/db_biopsies_trim_seq16S_noBCN.txt"
+meta_i <- read.delim(
+  file_meta_i, row.names = 1, check.names = FALSE,
+  stringsAsFactors = FALSE
+)
+file_meta_r <- file.path(rna, "metadata_25042018.csv")
+meta_r <- read.delim(
+  file_meta_r, check.names = FALSE,
+  stringsAsFactors = FALSE, 
+  na.strings = c("NA", "")
+)
+
+setwd(cd)
+
+# Correct the swapped samples
+position <- c(grep("33-T52-TTR-CIA", colnames(expr)), 
+              grep("33-T52-TTR-IIA", colnames(expr)))
+colnames(expr)[position] <- colnames(expr)[rev(position)]
+colnames(expr) <- toupper(colnames(expr))
+
+# Correct metadata
+meta_i <- meta_i_norm(meta_i)
+meta_r <- meta_r_norm(meta_r)
+
+# normalize names of samples
+colnames(otus_table_i) <- gsub("[0-9]+\\.(.+)$", "\\1", colnames(otus_table_i))
+
+# Check metadata with the names present in both datas
+meta_r <- meta_r[meta_r$Seq_code_uDNA %in% colnames(otus_table_i) &
+                   meta_r$`Sample Name_RNA` %in% colnames(expr), ]
+meta_r <- droplevels(meta_r)
+
+# Subset the sequencing data
+expr <- expr[, meta_r$`Sample Name_RNA`]
+otus_table_i <- otus_table_i[, meta_r$Seq_code_uDNA]
+
+# Translate into Entrez
+epithelium <- epitheliumE(epithelium)
+
+# Do the biological analysis of all the samples
+all_bootstrap <- readRDS("bootstrap.RDS")
+all_sgcca <- readRDS("sgcca.RDS")
+write.csv(integration::weights(all_sgcca), file = "RNAseq_weight_all.csv", na = "", row.names = FALSE)
+write.csv(integration::weights_otus(all_sgcca, otus_tax_i), file = "16S_weight_all.csv", na = "", row.names = FALSE)
+biological_relationships(all_sgcca, all_bootstrap, "all", otus_tax_i, epithelium, today)
+
+# Controls
+controls_bootstrap <- readRDS("bootstrap_Controls.RDS")
+controls_sgcca <- readRDS("Controls.RDS")
+write.csv(integration::weights(controls_sgcca), file = "RNAseq_weight_controls.csv", na = "", row.names = FALSE)
+write.csv(weights_otus(controls_sgcca, otus_tax_i), file = "16S_weight_controls.csv", na = "", row.names = FALSE)
+biological_relationships(controls_sgcca, controls_bootstrap, "controls", otus_tax_i, epithelium, today)
+
+# IBD
+IBD_bootstrap <- readRDS("bootstrap_IBD.RDS")
+IBD_sgcca <- readRDS("IBD.RDS")
+write.csv(integration::weights(IBD_sgcca), file = "RNAseq_weight_IBD.csv", na = "", row.names = FALSE)
+write.csv(weights_otus(IBD_sgcca, otus_tax_i), file = "16S_weight_IBD.csv", na = "", row.names = FALSE)
+biological_relationships(IBD_sgcca, IBD_bootstrap, "IBD", otus_tax_i, epithelium, today)
