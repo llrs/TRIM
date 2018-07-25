4129f6385ddb2185997738f4a42865a7e3a17af4
llrs <lluis.revilla@gmail.com>
2018-07-25 11:39:50 +0200

    Test the model with phenotype and interaction
    As discussed with Aida and Azucena

--- intestinal_16S_RNAseq_metadb/model2.R
+++ intestinal_16S_RNAseq_metadb/model2.R
@@ -1,5 +1,6 @@
 cd <- setwd("..")
 library("ggforce")
+library("RGCCA")
 
 # Load the helper file
 today <- format(Sys.time(), "%Y%m%d")
@@ -45,6 +46,8 @@ position <- c(grep("33-T52-TTR-CIA", colnames(expr)),
               grep("33-T52-TTR-IIA", colnames(expr)))
 colnames(expr)[position] <- colnames(expr)[rev(position)]
 colnames(expr) <- toupper(colnames(expr))
+#To match metadata
+colnames(expr) <- gsub("16-TM29", "16-TM30", colnames(expr)) 
 
 # normalize names of samples
 colnames(otus_table_i) <- gsub("[0-9]+\\.(.+)$", "\\1", colnames(otus_table_i))
@@ -59,8 +62,73 @@ meta_i <- meta_i_norm(meta_i)
 meta_r <- meta_r[meta_r$Seq_code_uDNA %in% colnames(otus_table_i) &
                    meta_r$`Sample Name_RNA` %in% colnames(expr), ]
 
-# Load the previously created data
-A <- readRDS(file = "TRIM.RDS")
+# Subset the sequencing data
+expr <- expr[, meta_r$`Sample Name_RNA`]
+otus_table_i <- otus_table_i[, meta_r$Seq_code_uDNA]
+
+# Normalize expression
+expr_edge <- edgeR::DGEList(expr)
+expr_edge <- edgeR::calcNormFactors(expr_edge, method = "TMM")
+expr_norm <- edgeR::cpm(expr_edge, normalized.lib.sizes=TRUE, log = TRUE)
+
+# Filter expression
+expr <- norm_RNAseq(expr_norm)
+
+# Normalize OTUS
+library("metagenomeSeq")
+MR_i <- newMRexperiment(
+  otus_table_i, 
+  featureData = AnnotatedDataFrame(as.data.frame(otus_tax_i[rownames(otus_table_i), ]))
+)
+MR_i <- cumNorm(MR_i, metagenomeSeq::cumNormStat(MR_i))
+otus_table_i <- MRcounts(MR_i, norm = TRUE, log = TRUE)
+
+# Subset if all the rows are 0 and if sd is 0
+otus_table_i <- otus_table_i[apply(otus_table_i, 1, sd) != 0, ]
+
+# Select the features of metadata Time and Age_sample isn't the same?? perhaps removing them
+metadb <- meta_r
+keepCol <- sapply(metadb, is.factor)
+nam <- c(
+  "Exact_location", # Segment of the sample
+  # superseeded by SESCD 
+  # "Active_area", # Health stage of the sample
+  # "IBD", # Disease or control
+  "AGE_SAMPLE", # Age
+  # "diagTime", # Time with disease
+  # Not really needed induced by diagTime and age sample
+  "AgeDiag", # Age at which the disease was diagnositcated
+  "Transplant", # Stage of the treatment
+  "ID", # Patient
+  # "SESCD_local", # Clinical score
+  "Treatment", # Further complications
+  "Surgery", # Up to surgery?
+  "SEX" # Male/female
+) 
+
+keepCol <- keepCol[nam]
+keepCol[nam] <- TRUE
+for (col in names(keepCol)) {
+  if (class(metadb[, col]) == "character") {
+    metadb[, col] <- as.factor(metadb[, col])
+    levels(metadb[, col]) <- seq_along(levels(metadb[, col]))
+  } else if (class(metadb[, col]) == "factor") {
+    levels(metadb[, col]) <- seq_along(levels(metadb[, col]))
+  } else if (class(metadb[, col]) == "numeric") {
+    next
+  }
+}
+metadb <- metadb[, names(keepCol)]
+
+# Set metadb with a sigle variable with several options
+metadb <- apply(metadb, 1:2, as.numeric)
+metadb[is.na(metadb)] <- 0
+
+# Prepare input for the sgcca function
+A <- list(RNAseq = t(expr), "16S" = t(otus_table_i), "metadata" = metadb)
+A <- sapply(A, function(x){
+  x[, apply(x, 2, sd) != 0]
+}, simplify = FALSE)
 
 # The design
 C <- matrix(
