b322cd20c066bc455b4922ac199f0f5fd9fd1a61
llrs <lluis.revilla@gmail.com>
2018-10-05 10:48:44 +0200

    Use precalculated values

--- intestinal_16S_RNAseq_integration/intestinal_16S_RNAseq_integration.R
+++ intestinal_16S_RNAseq_integration/intestinal_16S_RNAseq_integration.R
@@ -1,74 +1,18 @@
-cd <- setwd("..")
+library("ggforce")
+library("RGCCA")
 
 # Load the helper file
 today <- format(Sys.time(), "%Y%m%d")
 library("integration")
-library("RGCCA")
-
-intestinal <- "intestinal_16S"
-rna <- "intestinal_RNAseq"
+library("fgsea")
 
 
-# Read the intestinal otus table
-otus_table_i <- read.csv(
-  file.path(intestinal, "OTUs-Table-new-biopsies.csv"),
-  stringsAsFactors = FALSE, row.names = 1,
-  check.names = FALSE
-)
-tax_i <- otus_table_i[, ncol(otus_table_i)]
-otus_table_i <- otus_table_i[, -ncol(otus_table_i)]
+# Save
+otus_table_i <- readRDS("otus_table.RDS")
+otus_tax_i <- readRDS("otus_tax.RDS")
+expr <- readRDS("expr.RDS")
+meta_r <- readRDS( "meta.RDS")
 
-# Extract the taxonomy and format it properly
-otus_tax_i <- taxonomy(tax_i, rownames(otus_table_i))
-
-
-# Load the input data
-expr <- read.delim(file.path(rna, "taula_sencera2.tsv"), check.names = FALSE)
-
-# Read the metadata for each type of sample
-file_meta_i <- "intestinal_16S/db_biopsies_trim_seq16S_noBCN.txt"
-meta_i <- read.delim(
-  file_meta_i, row.names = 1, check.names = FALSE,
-  stringsAsFactors = FALSE
-)
-file_meta_r <- file.path(rna, "metadata_25042018.csv")
-meta_r <- read.delim(
-  file_meta_r, check.names = FALSE,
-  stringsAsFactors = FALSE, 
-  na.strings = c("NA", "")
-)
-
-setwd(cd)
-
-# Correct the swapped samples
-position <- c(grep("33-T52-TTR-CIA", colnames(expr)), 
-              grep("33-T52-TTR-IIA", colnames(expr)))
-colnames(expr)[position] <- colnames(expr)[rev(position)]
-colnames(expr) <- toupper(colnames(expr))
-#To match metadata
-colnames(expr) <- gsub("16-TM29", "16-TM30", colnames(expr)) 
-
-# Correct metadata
-meta_i <- meta_i_norm(meta_i)
-meta_r <- meta_r_norm(meta_r)
-
-# normalize names of samples
-colnames(otus_table_i) <- gsub("[0-9]+\\.(.+)$", "\\1", colnames(otus_table_i))
-
-# Check metadata with the names present in both datas
-meta_r <- meta_r[meta_r$Seq_code_uDNA %in% colnames(otus_table_i) &
-                   meta_r$`Sample Name_RNA` %in% colnames(expr), ]
-meta_r <- droplevels(meta_r)
-
-# Subset the sequencing data
-expr <- expr[, meta_r$`Sample Name_RNA`]
-otus_table_i <- otus_table_i[, meta_r$Seq_code_uDNA]
-
-# Filter expression
-expr <- norm_RNAseq(expr)
-
-#' Remove otus not present in that set of samples
-otus_table_i <- otus_table_i[rowSums(otus_table_i) > 0, ]
 
 # Prepare input for the sgcca function
 A <- list("RNAseq" = t(expr), "16S" = t(otus_table_i))
--- intestinal_16S_RNAseq_metadb/intestinal_16S_RNAseq_metadb.R
+++ intestinal_16S_RNAseq_metadb/intestinal_16S_RNAseq_metadb.R
@@ -1,90 +1,16 @@
-cd <- setwd("..")
 library("ggforce")
 library("RGCCA")
 
 # Load the helper file
 today <- format(Sys.time(), "%Y%m%d")
 library("integration")
-
 library("fgsea")
 
-intestinal <- "intestinal_16S"
-rna <- "intestinal_RNAseq"
-
-# Read the intestinal otus table
-otus_table_i <- read.csv(
-  file.path(intestinal, "OTUs-Table-new-biopsies.csv"),
-  stringsAsFactors = FALSE, row.names = 1,
-  check.names = FALSE
-)
-tax_i <- otus_table_i[, ncol(otus_table_i)]
-otus_table_i <- otus_table_i[, -ncol(otus_table_i)]
-
-# Extract the taxonomy and format it properly
-otus_tax_i <- taxonomy(tax_i, rownames(otus_table_i))
-
-# Load the input data
-expr <- read.delim(file.path(rna, "taula_sencera2.tsv"), check.names = FALSE)
-
-# Read the metadata for each type of sample
-file_meta_i <- "intestinal_16S/db_biopsies_trim_seq16S_noBCN.txt"
-meta_i <- read.delim(
-  file_meta_i, row.names = 1, check.names = FALSE,
-  stringsAsFactors = FALSE
-)
-file_meta_r <- file.path(rna, "metadata_25042018.csv")
-meta_r <- read.delim(
-  file_meta_r, check.names = FALSE,
-  stringsAsFactors = FALSE, 
-  na.strings = c("NA", "")
-)
-
-setwd(cd)
-
-# Correct the swapped samples
-position <- c(grep("33-T52-TTR-CIA", colnames(expr)), 
-              grep("33-T52-TTR-IIA", colnames(expr)))
-colnames(expr)[position] <- colnames(expr)[rev(position)]
-colnames(expr) <- toupper(colnames(expr))
-#To match metadata
-colnames(expr) <- gsub("16-TM29", "16-TM30", colnames(expr)) 
-
-# normalize names of samples
-colnames(otus_table_i) <- gsub("[0-9]+\\.(.+)$", "\\1", colnames(otus_table_i))
-
-# Normalize the RNA metadata
-meta_r <- meta_r_norm(meta_r)
-
-# Normalize the 16S intestinal metadata
-meta_i <- meta_i_norm(meta_i)
-
-# Check metadata with the names present in both datas
-meta_r <- meta_r[meta_r$Seq_code_uDNA %in% colnames(otus_table_i) &
-                   meta_r$`Sample Name_RNA` %in% colnames(expr), ]
-
-# Subset the sequencing data
-expr <- expr[, meta_r$`Sample Name_RNA`]
-otus_table_i <- otus_table_i[, meta_r$Seq_code_uDNA]
-
-# Normalize expression
-expr_edge <- edgeR::DGEList(expr)
-expr_edge <- edgeR::calcNormFactors(expr_edge, method = "TMM")
-expr_norm <- edgeR::cpm(expr_edge, normalized.lib.sizes=TRUE, log = TRUE)
-
-# Filter expression
-expr <- norm_RNAseq(expr_norm)
-
-# Normalize OTUS
-library("metagenomeSeq")
-MR_i <- newMRexperiment(
-  otus_table_i, 
-  featureData = AnnotatedDataFrame(as.data.frame(otus_tax_i[rownames(otus_table_i), ]))
-)
-MR_i <- cumNorm(MR_i, metagenomeSeq::cumNormStat(MR_i))
-otus_table_i <- MRcounts(MR_i, norm = TRUE, log = TRUE)
-
-# Subset if all the rows are 0 and if sd is 0
-otus_table_i <- otus_table_i[apply(otus_table_i, 1, sd) != 0, ]
+# Load data
+otus_table_i <- readRDS("otus_table.RDS")
+otus_tax_i <- readRDS("otus_tax.RDS")
+expr <- readRDS("expr.RDS")
+meta_r <- readRDS("meta.RDS")
 
 # Select the features of metadata Time and Age_sample isn't the same?? perhaps removing them
 metadb <- meta_r
@@ -166,28 +92,28 @@ names(sgcca.centroid$astar) <- names(A)
 names(sgcca.centroid$AVE$AVE_X) <- names(A)
 sgcca.centroid$AVE$AVE_X <- simplify2array(sgcca.centroid$AVE$AVE_X)
 sgcca.centroid$AVE
-
-sgcca.factorial <- sgcca(
-  A, C, c1 = shrinkage,
-  ncomp = ncomp,
-  scheme = "factorial",
-  scale = TRUE,
-  verbose = FALSE
-)
-names(sgcca.factorial$Y) <- names(A)
-names(sgcca.factorial$a) <- names(A)
-names(sgcca.factorial$astar) <- names(A)
-
-sgcca.horst <- sgcca(
-  A, C, c1 = shrinkage,
-  ncomp = ncomp,
-  scheme = "horst",
-  scale = TRUE,
-  verbose = FALSE
-)
-names(sgcca.horst$Y) <- names(A)
-names(sgcca.horst$a) <- names(A)
-names(sgcca.horst$astar) <- names(A)
+# 
+# sgcca.factorial <- sgcca(
+#   A, C, c1 = shrinkage,
+#   ncomp = ncomp,
+#   scheme = "factorial",
+#   scale = TRUE,
+#   verbose = FALSE
+# )
+# names(sgcca.factorial$Y) <- names(A)
+# names(sgcca.factorial$a) <- names(A)
+# names(sgcca.factorial$astar) <- names(A)
+# 
+# sgcca.horst <- sgcca(
+#   A, C, c1 = shrinkage,
+#   ncomp = ncomp,
+#   scheme = "horst",
+#   scale = TRUE,
+#   verbose = FALSE
+# )
+# names(sgcca.horst$Y) <- names(A)
+# names(sgcca.horst$a) <- names(A)
+# names(sgcca.horst$astar) <- names(A)
 
 # list(sgcca.centroid = sgcca.centroid, sgcca.horst = sgcca.horst,
 # sgcca.factorial = sgcca.factorial)
