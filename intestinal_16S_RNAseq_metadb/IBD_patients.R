b08b483907c11f4c72e5d8eef1e059e62abd60bf
llrs <lluis.revilla@gmail.com>
2018-10-05 11:25:56 +0200

    Use normalized values after subsetting

--- intestinal_16S_RNAseq_metadb/IBD_patients.R
+++ intestinal_16S_RNAseq_metadb/IBD_patients.R
@@ -1,69 +1,15 @@
-cd <- setwd("..")
-library("fgsea")
 library("ggforce")
 library("RGCCA")
+
 # Load the helper file
 today <- format(Sys.time(), "%Y%m%d")
 library("integration")
+library("fgsea")
 
-intestinal <- "intestinal_16S"
-rna <- "intestinal_RNAseq"
-
-
-# Read the intestinal otus table
-otus_table_i <- read.csv(
-  file.path(intestinal, "OTUs-Table-new-biopsies.csv"),
-  stringsAsFactors = FALSE, row.names = 1,
-  check.names = FALSE
-)
-tax_i <- otus_table_i[, ncol(otus_table_i)]
-otus_table_i <- otus_table_i[, -ncol(otus_table_i)]
-
-# Extract the taxonomy and format it properly
-otus_tax_i <- taxonomy(tax_i, rownames(otus_table_i))
-
-
-# Load the input data
-expr <- read.delim(file.path(rna, "taula_sencera2.tsv"), check.names = FALSE)
-
-# Read the metadata for each type of sample
-file_meta_i <- "intestinal_16S/db_biopsies_trim_seq16S_noBCN.txt"
-meta_i <- read.delim(
-  file_meta_i, row.names = 1, check.names = FALSE,
-  stringsAsFactors = FALSE
-)
-file_meta_r <- file.path(rna, "metadata_25042018.csv")
-meta_r <- read.delim(
-  file_meta_r, check.names = FALSE,
-  stringsAsFactors = FALSE, 
-  na.strings = c("NA", "")
-)
-
-setwd(cd)
-
-# Correct the swapped samples
-position <- c(grep("33-T52-TTR-CIA", colnames(expr)), 
-              grep("33-T52-TTR-IIA", colnames(expr)))
-colnames(expr)[position] <- colnames(expr)[rev(position)]
-colnames(expr) <- toupper(colnames(expr))
-
-# Correct the metadata
-meta_i <- meta_i_norm(meta_i)
-meta_r <- meta_r_norm(meta_r)
-
-# normalize names of samples
-colnames(otus_table_i) <- gsub("[0-9]+\\.(.+)$", "\\1", colnames(otus_table_i))
-
-# Check metadata with the names present in both datas
-meta_r <- meta_r[meta_r$Seq_code_uDNA %in% colnames(otus_table_i) &
-                   meta_r$`Sample Name_RNA` %in% colnames(expr), ]
-
-# Filter only the IBD patients
-meta_r <- meta_r[meta_r$IBD == "CD", ]
-
-# Subset the sequencing data
-expr <- expr[, meta_r$`Sample Name_RNA`]
-otus_table_i <- otus_table_i[, meta_r$Seq_code_uDNA]
+# Load data
+otus_table_i <- readRDS("IBD_otus_table.RDS")
+expr <- readRDS("IBD_expr.RDS")
+meta_r <- readRDS("meta.RDS")
 
 # Select the features of metadata Time and Age_sample isn't the same?? perhaps removing them
 metadb <- meta_r
