f7b0b138a38c6337d0bea6a262401b7140e3a9fd
Lluís Revilla <lluis.revilla@gmail.com>
2018-12-04 10:15:24 +0100

    Calculate position of the centroid

--- intestinal_16S_RNAseq_metadb/distances_groups.R
+++ intestinal_16S_RNAseq_metadb/distances_groups.R
@@ -1,5 +1,6 @@
 library("ggforce")
 library("RGCCA")
+library("Matrix")
 
 # Load the helper file
 today <- format(Sys.time(), "%Y%m%d")
@@ -18,3 +19,25 @@ levels(as.factor(meta_r$Location))
 levels(as.factor(meta_r$IBD))
 keep <- allComb(meta_r, c("Location", "IBD"))
 
+model <- readRDS("model3_best.RDS")
+
+relevantOTUs <- rownames(model$a[[2]])[model$a[[2]][, 1] != 0]
+relevantGenes <- rownames(model$a[[1]])[model$a[[1]][, 1] != 0]
+otus <- otus_table_i[relevantOTUs, ]
+expr_f <- expr[relevantGenes, ]
+
+# Calculate the center of each group
+out <- lapply(as.data.frame(keep), function(x) {
+  Y <- rowMeans(otus[, x], na.rm = TRUE)
+  X <- rowMeans(expr_f[, x], na.rm = TRUE)
+  list("Y" = Y, "X" = X)
+})
+
+
+points <- lapply(out, fuction(x) {
+  for (o in x$Y) {
+    for (i in x$X) {
+      c(o, i)
+    }
+  }
+})
