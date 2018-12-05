c456aefd37b2226da436815fe1ed76082b9de31f
Lluís Revilla <lluis.revilla@gmail.com>
2018-12-05 11:59:54 +0100

    Introduce GSEA to check if the distance are meaningful

--- intestinal_16S_RNAseq_metadb/distances_groups.R
+++ intestinal_16S_RNAseq_metadb/distances_groups.R
@@ -1,7 +1,11 @@
+library("gghighlight")
+library("org.Hs.eg.db")
 library("ggplot2")
 library("ggforce")
 library("RGCCA")
 library("Matrix")
+library("tidyr")
+library("dplyr")
 
 # Load the helper file
 today <- format(Sys.time(), "%Y%m%d")
@@ -125,9 +129,84 @@ pic <- simplify2array(picky)
 taxa <- otus_tax_i[pic[1, ], 6:7]
 taxa[is.na(taxa)] <- ""
 taxes <- apply(taxa, 1, paste0, collapse = " ")
-library("org.Hs.eg.db")
+
 g <- trimVer(pic[2, ])
 g2 <- mapIds(org.Hs.eg.db, keys = g, keytype = "ENSEMBL", column = "SYMBOL")
 
 paired <- cbind.data.frame("Micro" = taxes, "Gene" = g2)
 saveRDS(paired, "gene_microorganism.RDS")
+
+genes <- vapply(pairs, function(x){
+  x[2]
+}, character(1L))
+tax <- vapply(pairs, function(x){
+  x[1]
+}, character(1L))
+
+ga <- mapIds(org.Hs.eg.db, keys = trimVer(genes), keytype = "ENSEMBL", column = "SYMBOL")
+genus <- otus_tax_i[tax, 6:7]
+genus[is.na(genus)] <- ""
+genus <- apply(genus, 1, paste0, collapse = " ")
+denrich <- cbind(d, Micro = genus, Gene = ga, Ensembl = trimVer(genes))
+ggplot(denrich) +
+  geom_point(aes(ICD_CCt, CCD_ICt, color = Micro), alpha = 0.1) +
+  geom_abline(slope = 1, intercept = 0, col = "red") +
+  # gghighlight(ICD_CCt > 10) +
+  guides(color = FALSE)
+
+droplets <- denrich %>%
+  filter(ICD_CCt > 7.5 & CCD_ICt >7.5) %>% 
+  group_by(Gene) %>% 
+  summarise(ICD_CCt_max = max(ICD_CCt),
+            ICD_CCt_min = min(ICD_CCt),
+            CCD_ICt_max = max(CCD_ICt),
+            CCD_ICt_min = min(CCD_ICt)
+            )
+droplets2 <- denrich %>% 
+  filter(ICD_CCt > 7.5 | CCD_ICt > 7.5)
+write.csv(droplets2, file = "distancies_meteorits.csv")
+
+a <- denrich %>% 
+  group_by(Gene) %>% 
+  count(n_distinct(Ensembl)) %>% 
+  ungroup()
+
+keep <- denrich$ICD_CCt > 7.5 | denrich$CCD_ICt > 7.5
+keep_f <- function(x){
+  denrich$ICD_CCt > x | denrich$CCD_ICt > x
+}
+sapply(1:13, function(x){length(unique(denrich$Gene[keep_f(x)]))})
+ggplot(droplevels(denrich)) +
+  geom_point(aes(ICD_CCt, CCD_ICt, color = "grey"), alpha = 0.2) +
+  geom_point(data = denrich[keep, ], aes(ICD_CCt, CCD_ICt, color = Gene)) +
+  geom_abline(slope = 1, intercept = 0, col = "red") +
+  guides(color = FALSE)
+keep2 <- denrich$ICD_CCt > 10.5 & denrich$CCD_ICt < 5
+ggplot(droplevels(denrich)) +
+  geom_point(aes(ICD_CCt, CCD_ICt, color = "grey"), alpha = 0.2) +
+  geom_point(data = denrich[keep2, ], aes(ICD_CCt, CCD_ICt, color = Gene)) +
+  geom_abline(slope = 1, intercept = 0, col = "red") +
+  guides(color = FALSE)
+
+rank <- rowSums(denrich[, 1:6])
+library("fgsea")
+names(rank) <- as.character(seq_along(rank))
+nam <- split(seq_along(rank), denrich$Micro)
+nam <- nam[-which(names(nam) == " ")] # Remove the empty one
+microEnrich <- fgsea(nam, stats = rank, nperm = 10000)
+microEnrich %>% 
+  filter(padj < 0.05) %>% 
+  arrange(padj, desc(abs(NES))) %>% 
+  select(-leadingEdge, -size) %>% 
+  as_tibble()
+
+nam2 <- split(seq_along(rank), denrich$Gene)
+geneEnrich <- fgsea(nam2, stats = rank, nperm = 10000)
+geneEnrich %>% 
+  filter(padj < 0.05) %>% 
+  arrange(padj, desc(abs(NES))) %>% 
+  select(-leadingEdge, -size) %>% 
+  as_tibble()
+si <- geneEnrich %>% 
+  filter(padj < 0.05)
+hist(si$NES)
