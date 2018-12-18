117080978bc439acba4244d4810800e57b93fb75
llrs <lluis.revilla@gmail.com>
2018-12-18 17:44:39 +0100

    Changing the shrinkage to the optimutm :exclamation:
    This breaks previous assumptions that model 0 has a lot less genes!!
    Why it is so dramatic impact on the number of features!!

--- intestinal_16S_RNAseq_integration/intestinal_16S_RNAseq_integration.R
+++ intestinal_16S_RNAseq_integration/intestinal_16S_RNAseq_integration.R
@@ -1,17 +1,16 @@
 library("ggforce")
 library("RGCCA")
+library("integration")
+library("fgsea")
 
 # Load the helper file
 today <- format(Sys.time(), "%Y%m%d")
-library("integration")
-library("fgsea")
 
 
 # Save
 otus_table_i <- readRDS("otus_table.RDS")
 expr <- readRDS("expr.RDS")
 meta_r <- readRDS( "meta.RDS")
-
 
 # Prepare input for the sgcca function
 A <- list("RNAseq" = t(expr), "16S" = t(otus_table_i))
@@ -28,7 +27,7 @@ model0i <- subSymm(model0, 1, 1, 1)
 
 # We cannnot comput eht tau.estimate for A[[1]]
 # (shrinkage <- sapply(A, tau.estimate))
-shrinkage <- c(0.1, 0) # We guess a 0.1
+shrinkage <- c(0.25670333, 0) # We guess a 0.1
 shrinkage[2] <- tau.estimate(A[[2]])
 (min_shrinkage <- sapply(A, function(x) {
   1 / sqrt(ncol(x))
@@ -45,7 +44,9 @@ sgcca.centroid <- sgcca(
   scale = TRUE,
   verbose = FALSE
 )
-saveRDS(sgcca.centroid, "sgcca_i.RDS")
+sgcca.centroid <- improve.sgcca(sgcca.centroid, names(A))
+beepr::beep()
+saveRDS(sgcca.centroid, "sgccai.RDS")
 sgcca.centroid <- sgcca(
   A, C = model0, c1 = shrinkage,
   ncomp = ncomp,
@@ -75,10 +76,9 @@ dist <- apply(samples, 1, dist2d, d = d)
 names(colors) <- unique(meta_r$ID)
 
 samples <- cbind(samples, meta_r, "dist" = dist)
-samples$Patient_ID <- as.factor(samples$Patient_ID)
-samples$Sample_Code <- as.character(samples$Sample_Code)
 
-pdf(paste0("Figures/", today, "_RGCCA_plots.pdf"))
+# Plots ####
+# pdf(paste0("Figures/", today, "_RGCCA_plots.pdf"))
 
 # Plot if the coherence between samples has a specific pattern
 ggplot(samples) +
@@ -87,190 +87,27 @@ ggplot(samples) +
 
 hist(samples$dist)
 
-# Labels of the samples
-label <- strsplit(as.character(samples$`Sample Name_RNA`), split = "-")
-labels <- sapply(label, function(x) {
-  if (length(x) == 5) {
-    x[5]
-  }
-  else if (length(x) != 5) {
-    x[4]
-  }
-})
+plot_samples(samples, colors)
 
-samples <- cbind(samples, labels)
-samples$Time <- factor(samples$Time, levels(as.factor(samples$Time))[c(1, 2, 4, 5, 3, 6, 7, 8)])
+variables <- variables(sgcca)
 
-# Some common structure of plots
-comm <- ggplot(samples, aes(RNAseq, Micro)) + # It is really biopsies
-  geom_vline(xintercept = 0) +
-  geom_hline(yintercept = 0) +
-  ggtitle("All samples at all times ") +
-  xlab("RNAseq (component 1)") +
-  ylab("16S (component 1)") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  geom_abline(intercept = 0, slope = d[1], linetype = 2)
-
-for (p in seq_along(levels(samples$Time))) {
-  a <- comm +
-    geom_text(aes(color = ID, label = ID)) +
-    geom_vline(xintercept = 0) +
-    geom_hline(yintercept = 0) +
-    guides(col = guide_legend(title = "Patient")) +
-    scale_color_manual(values = colors) +
-    facet_wrap_paginate(~Time, ncol = 1, nrow = 1, page = p)
-  print(a)
-}
-
-for (p in seq_along(levels(samples$ID))) {
-  a <- comm +
-    geom_text(aes(color = ID, label = ifelse(!is.na(labels),
-      paste(Time, labels, sep = "_"),
-      as.character(Time)
-    ))) +
-    guides(col = guide_legend(title = "Patient")) +
-    scale_color_manual(values = colors) +
-    facet_wrap_paginate(~ID, ncol = 1, nrow = 1, page = p)
-  print(a)
-}
-
-comm +
-  geom_text(aes(
-    color = ID,
-    label = ifelse(!is.na(labels),
-      paste(Time, labels, sep = "_"),
-      as.character(Time)
-    )
-  )) +
-  guides(col = guide_legend(title = "Patient")) +
-  scale_color_manual(values = colors)
-
-
-comm +
-  geom_text(aes(
-    color = HSCT_responder,
-    label = ifelse(!is.na(labels),
-      paste(ID, labels, sep = "_"),
-      as.character(Time)
-    )
-  )) +
-  guides(col = guide_legend(title = "Responders"))
-
-
-comm +
-  geom_text(aes(
-    color = Endoscopic_Activity,
-    label = ifelse(!is.na(labels),
-      paste(ID, labels, sep = "_"),
-      as.character(ID)
-    )
-  )) +
-  guides(col = guide_legend(title = "Endoscopic Activity"))
-comm +
-  geom_text(aes(
-    color = Exact_location,
-    label = ifelse(!is.na(labels),
-                   paste(ID, labels, sep = "_"),
-                   as.character(ID)
-    )
-  )) +
-  guides(col = guide_legend(title = "Location"))
-
-comm +
-  geom_text(aes(color = Time, 
-                label = ifelse(!is.na(labels),
-                               paste(ID, labels, sep = "_"),
-                               as.character(ID)
-                ))) +
-  guides(col = guide_legend(title = "Time"))
-
-comm +
-  geom_text(aes(color = SESCD_local, 
-                label = ifelse(!is.na(labels),
-                               paste(ID, labels, sep = "_"),
-                               as.character(ID)
-                ))) +
-  guides(col = guide_legend(title = "SESCD (local)"))
-
-comm +
-  geom_text(aes(color = IBD, label = as.character(ID))) +
-  guides(col = guide_legend(title = "Type"))
-
-variables <- data.frame(
-  comp1 = unlist(sapply(
-    sgcca.centroid$a,
-    function(x) {
-      x[, 1]
-    }
-  )),
-  comp2 = unlist(sapply(
-    sgcca.centroid$a,
-    function(x) {
-      x[, 2]
-    }
-  )),
-  Origin = rep(names(A), sapply(A, ncol))
-)
-variables$var <- gsub("^.*\\.(OTU_.*)$", "\\1", rownames(variables))
-rownames(variables) <- gsub("^.*\\.(OTU_.*)$", "\\1", rownames(variables))
-variables$var <- gsub("^RNAseq\\.(ENSG.*)$", "\\1", rownames(variables))
-rownames(variables) <- gsub("^.*\\.(ENSG.*)$", "\\1", rownames(variables))
-
-# Remove the variables that in both components are 0
-keepComp1RNAseq <- mean(abs(variables$comp1)[variables$Origin == "RNAseq"])
-keepComp1_16S <- mean(abs(variables$comp1)[variables$Origin != "RNAseq"])
-
-keepComp2RNAseq <- mean(abs(variables$comp2)[variables$Origin == "RNAseq"])
-keepComp2_16S <- mean(abs(variables$comp2)[variables$Origin != "RNAseq"])
-
-keepComp1 <- c(
-  variables$comp1[variables$Origin == "RNAseq"] > keepComp1RNAseq,
-  variables$comp1[variables$Origin != "RNAseq"] > keepComp1_16S
-)
-keepComp2 <- c(
-  variables$comp2[variables$Origin == "RNAseq"] > keepComp2RNAseq,
-  variables$comp2[variables$Origin != "RNAseq"] > keepComp2_16S
-)
-subVariables <- variables[keepComp1 | keepComp2, ]
-
-ggplot(subVariables, aes(comp1, comp2), color = Origin) +
-  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.1, npoints = 100)) +
-  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.2, npoints = 100)) +
-  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.3, npoints = 100)) +
-  geom_path(aes(x, y), data = circleFun(c(0, 0), 0.4, npoints = 100)) +
-  geom_text(aes(color = Origin, label = var)) +
-  geom_vline(xintercept = 0) +
-  geom_hline(yintercept = 0) +
-  coord_cartesian() +
-  ggtitle(
-    "Variables important for the first two components",
-    subtitle = "Integrating stools and mucosa samples"
-  )
+plot_variables(variables)
 
 # Plot for the same component the variables of each block
-comp1 <- sapply(sgcca.centroid$a, function(x) {
-  x[, 1]
-})
+comp1 <- lapply(sgcca.centroid$a, function(x) {x[, 1]})
 variables_weight(comp1)
 
 # Second component
-comp2 <- sapply(sgcca.centroid$a, function(x) {
-  x[, 2]
-})
+comp2 <- lapply(sgcca.centroid$a, function(x) {x[, 2]})
 variables_weight(comp2)
 
 # Validate #### 
 l <- looIndex(size(A))
-loo_model <- function(x, model) {
-  
-  RGCCA::sgcca(A = subsetData(A, x),
-               C = model, 
-               scheme = "centroid", 
-               verbose = FALSE, c1 = shrinkage)
-}
-result.out <- lapply(l, loo_model, model = model0) # SGCCA of the selected model leaving one sample each time out of order.
+loo_model <- loo_functions(A, shrinkage)
+# SGCCA of the selected model leaving one sample each time out of order.
+result.out <- lapply(l, loo_model, model = model0) 
 saveRDS(result.out, "loo-model0.RDS")
-result.out <- lapply(l, loo_model, model = model0i) # SGCCA of the selected model leaving one sample each time out of order.
+result.out <- lapply(l, loo_model, model = model0i) 
 saveRDS(result.out, "loo-model0i.RDS")
 
 # Bootstrap of sgcca
