b35340b19c9009edf44ed9e65aa58e913c64ef4e
llrs <lluis.revilla@gmail.com>
2019-01-07 12:39:36 +0100

    Iterate over similar models as in model3_best

--- intestinal_16S_RNAseq_metadb/non_overfitting.R
+++ intestinal_16S_RNAseq_metadb/non_overfitting.R
@@ -1,5 +1,5 @@
 library("ggforce")
-library("RGCCA")
+library("RGCCA2")
 library("integration")
 library("fgsea")
 library("dplyr")
@@ -8,15 +8,18 @@ library("dplyr")
 today <- format(Sys.time(), "%Y%m%d")
 
 A <- readRDS("model3_TRIM.RDS")
+meta_r <- readRDS( "meta.RDS")
 
 # We cannnot comput eht tau.estimate for A[[1]] calculated on server
+# original value 0.25670333
+# last calculated value is of 0.249488046688595
 shrinkage <- c(0.249488046688595, 0, 1, 1, 1) 
 shrinkage[[2]] <- tau.estimate(A[[2]]) # 0.286506412433534
 
 # Experiment design for the complicated cases 
 # with too many computations possible to perform it only tests for 3 weighs per edge
 designs <- weight_design(weights = 3, size = 5)
-keep <- vapply(designs, correct, logical(1L))
+keep <- vapply(designs, RGCCA2::correct, logical(1L))
 designs <- designs[keep]
 
 # Step 1 ####
@@ -26,7 +29,7 @@ set.seed(4672679)
 s <- sample(designs, size = min(length(designs)*.1, 1000))
 # Perform the sgcca on these samples
 testing <- function(x, type, ...) {
-  result.sgcca <- RGCCA::sgcca(C = x, 
+  result.sgcca <- RGCCA2::sgcca(C = x, 
                                scheme = type, 
                                verbose = FALSE, 
                                scale = FALSE,
@@ -95,12 +98,42 @@ saveRDS(best_interaction, "model3_best_interaction.RDS")
 
 result.i <- lapply(l, function(x){
   
-  RGCCA::sgcca(A = subsetData(A, x),
+  RGCCA2::sgcca(A = subsetData(A, x),
                C = model3_best2, 
                scheme = "centroid", 
-               verbose = FALSE, c1 = shrinkage
+               verbose = FALSE, 
+               c1 = shrinkage
   )}) # SGCCA of the selected model leaving one sample each time out of order.
 saveRDS(result.i, "loo-model3_best_interaction.RDS")
 a <- best$a[[1]][, 1]
 ai <-  best_interaction$a[[1]][, 1]
 hist(a[a != 0 | ai != 0] - ai[a != 0 | ai != 0]) # Normal distribution: randomness
+
+
+## 
+model3_best <- readRDS("model3_best.RDS")
+C3 <- model3_best$C
+diff_0 <- lower.tri(C3) & C3 != 0
+like_3best <- weight_design(weights = 11, size = 5, which(diff_0))
+keep_subset <- vapply(like_3best, correct, FUN.VALUE = logical(1L))
+d <- like_3best[keep_subset]
+
+# Estimated time of 8 hours
+out <- sapply(d, testing, type = "centroid", A = Ab, c1 = shrinkage, USE.NAMES = FALSE)
+out2 <- as.data.frame(t(out))
+saveRDS(out2, "iterations_model3b.RDS")
+beep()
+out2 %>% 
+  top_n(5, AVE_inner) %>% 
+  select(AVE_inner, AVE_outer, var12, var13, var23, 
+         var14, var24, var34, var15, var25, var35, var45) %>% 
+  arrange(desc(AVE_inner))
+model3bb <- symm(C3, out2[out2$AVE_inner == max(out2$AVE_inner), 
+                          grep("^var", colnames(out2))])
+model3bb
+out2 %>% 
+  filter(AVE_inner > model3_best$AVE$AVE_inner[1]) %>% 
+  count()
+sgcca_model3bb <- sgcca(Ab, c1 = shrinkage, scheme = "centroid", C = model3bb)
+sgcca_model3bb <- improve.sgcca(sgcca_model3bb, names(Ab))
+saveRDS(sgcca_model3bb, "model3_best2.RDS")
