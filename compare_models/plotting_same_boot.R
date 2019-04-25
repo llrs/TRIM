8208cfc1dc3319afaa817eb9469ca9e19821797a
llrs <lluis.revilla@gmail.com>
2019-04-25 09:23:00 +0200

    Look for correlation in genes-microorganism

--- compare_models/plotting_same_boot.R
+++ compare_models/plotting_same_boot.R
@@ -4,22 +4,21 @@ folder <- "." # intestinal_16S_RNAseq_integration
 b0 <- readRDS(file.path(folder, "boot0.RDS"))
 b1.2 <- readRDS(file.path(folder, "boot1.2.RDS"))
 b2.2 <- readRDS(file.path(folder, "boot2.2.RDS"))
-pb0 <- sort(colSums(b0$STAB[[1]] != 0)/1000)
-color <- "red"
-barplot(pb0[pb0 != 0], col = color, xlab = "Genes", ylab = "Probability", 
-        main = "Genes in model 0", border = color)
-pb1.2 <- sort(colSums(b1.2$STAB[[1]] != 0)/1000)
-color <- "green"
-barplot(pb1.2[pb1.2 != 0], col = color, xlab = "Genes", ylab = "Probability", 
-        main = "Genes in model 1.2", border = color)
-pb2.2 <- sort(colSums(b2.2$STAB[[1]] != 0)/1000)
-color <- "blue"
-barplot(pb2.2[pb2.2 != 0], col = color, xlab = "Genes", ylab = "Probability", 
-        main = "Genes in model 2.2", border = color)
-# compare
-hist(b0$AVE[, 1])
-hist(b1.2$AVE[, 1])
-hist(b2.2$AVE[, 1])
+
+# Models ###
+folder0 <- "../intestinal_16S_RNAseq_integration"
+model0 <- readRDS(file.path(folder0, "sgcca.RDS"))
+
+# model 1 without interaction
+folder1 <- "../intestinal_16S_RNAseq_metadb"
+# model 2 without interaction
+model1.2 <- readRDS(file.path(folder1, "model2_best.RDS"))
+# model 3
+model2.2 <- readRDS(file.path(folder1, "model3_wo_forced_interaction.RDS"))
+
+RNAseq <- readRDS(file.path(folder1, "expr.RDS"))
+
+# AVE ####
 AVE0 <- t(sapply(b0, "[[", "AVE"))
 AVE1.2 <- t(sapply(b1.2, "[[", "AVE"))
 AVE2.2 <- t(sapply(b2.2, "[[", "AVE"))
@@ -27,6 +26,8 @@ b <- rbind.data.frame(cbind.data.frame(AVE0, model = "0"),
                       cbind.data.frame(AVE1.2, model = "1.2"),
                       cbind.data.frame(AVE2.2, model = "2.2"))
 b$index <- rep(seq_len(1000), 3)
+# This index doesn't converge
+b <- b[!(b$inner == 0 & b$outer == 0), ]
 ggplot(b) + 
   geom_density(aes(inner, group = model, fill = model), alpha = 0.5)
 ggplot(b) + 
@@ -46,7 +47,7 @@ ggplot(b) +
              col = "red") +
   labs(title = "AVE in bootstraps")
 
-i <- sapply(index, function(x)x$index)
+i <- sapply(index, function(x)x)
 t_i <- sort(table(i)) - 1000
 barplot(t_i, main = "Deviation from 1000", 
         col = as.factor(meta$Patient_ID[as.numeric(names(t_i))]))
@@ -72,13 +73,13 @@ ggplot(meta2) +
        title = "Samples repeated in bootstrapping",
        col = "Disease", fill = "Disease")
 
-meta_r <- readRDS("intestinal_16S_RNAseq_metadb//meta.RDS")
+meta_r <- readRDS("../intestinal_16S_RNAseq_metadb//meta.RDS")
 df <- sapply(index, function(i, x) {
   
-  c(Controls = sum(x$IBD[i$index] != "CD")/158,
-    Ileum = sum(x$Exact_location[i$index] == "ILEUM", na.rm = TRUE)/158,
-    Smoker = sum(x$Tobacco[i$index] == "No", na.rm = TRUE)/158,
-    Age = mean(x$AGE_SAMPLE[i$index]),
+  c(Controls = sum(x$IBD[i] != "CD")/158,
+    Ileum = sum(x$Exact_location[i] == "ILEUM", na.rm = TRUE)/158,
+    Smoker = sum(x$Tobacco[i] == "No", na.rm = TRUE)/158,
+    Age = mean(x$AGE_SAMPLE[i]),
     Female = sum(x$SEX == "FEMALE")/158
   )
 }, x = meta_r)
@@ -115,3 +116,35 @@ CA <- ggplot(df) +
        y = "Age (Mean)",
        caption = "HSCT cohort")
 CC/CA
+
+# microbiome ####
+micro_fun <- function(x){x$STAB$`16S`}
+micro2.2 <- lapply(b2.2, micro_fun)
+micro2.2 <- micro2.2[-230]
+
+dna <- matrix(0, nrow = nrow(model0$a$`16S`), ncol = 999)
+rownames(dna) <- rownames(model0$a$`16S`)
+i <- 1
+for (mod in micro2.2) {
+  dna[, i][names(mod)] <- mod
+  i <- i + 1
+}
+
+# transcriptome ####
+rna_fun <- function(x){x$STAB$RNAseq}
+rna2.2 <- lapply(b2.2, rna_fun)
+rna2.2 <- rna2.2[-230]
+
+rna <- matrix(0, nrow = nrow(RNAseq), ncol = 999)
+rownames(rna) <- rownames(RNAseq)
+i <- 1
+for (mod in rna2.2) {
+  rna[, i][names(mod)] <- mod
+  i <- i + 1
+}
+
+cors <- WGCNA::cor(t(rna), t(dna), 
+                   method = "spearman", 
+                   use = "pairwise.complete.obs")
+
+cors <- cor(t(rna), t(dna), use = "pairwise.complete.obs")
